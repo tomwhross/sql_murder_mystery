92b04fca01bfaabbaaf312268189bb45f96d091f
tomwhross <69170368+tomwhross@users.noreply.github.com>
2020-10-04 08:48:31 -0400

    Create 11_solution_2.sql

---
+++ 11_solution_2.sql
@@ -0,0 +1,12 @@
+/*
+  Solution for tracing the real killter (the person who hired the killer)
+/*
+
+INSERT INTO solution VALUES (1, 'Miranda Priestly');
+        
+  SELECT value FROM solution;
+        
+/*
+  Returns one record, with value equal to
+    'Congrats, you found the brains behind the murder! Everyone in SQL City hails you as the greatest SQL detective of all time. Time to break out the champagne!'
+*/
