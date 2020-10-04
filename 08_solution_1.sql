43ceee202b829f30bff392f99515b92649afeb19
tomwhross <69170368+tomwhross@users.noreply.github.com>
2020-10-04 08:15:22 -0400

    Create 08_solution_1.sql

---
+++ 08_solution_1.sql
@@ -0,0 +1,12 @@
+/*
+  Just pasting the person.name value from the pervious result set into the provided template
+*/
+
+INSERT INTO solution VALUES (1, 'Jeremy Bowers');
+        
+  SELECT value FROM solution;
+  
+/*
+  Result set contains the following value:
+    'Congrats, you found the murderer! But wait, there's more... If you think you're up for a challenge, try querying the interview transcript of the murderer to find the real villain behind this crime. If you feel especially confident in your SQL skills, try to complete this final step with no more than 2 queries. Use this same INSERT statement with your new suspect to check your answer.'
+*/
